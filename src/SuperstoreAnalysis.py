e9812a3ef643bc99c90b74f5aebeebe6e7bd05fe
vidhan-79 <vidhanrathod000@gmail.com>
2026-08-19 19:32:22 +0530

    Tests and rough answers
    This is the version i want to save before i fix and format whole files

--- src/SuperstoreAnalysis.py
+++ src/SuperstoreAnalysis.py
@@ -4,6 +4,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+pd.set_option("display.float_format", "{:,.2f}".format)
+
 # =========================
 #  LOAD DATA
 # =========================
@@ -18,11 +20,13 @@ super_store_df = pd.read_csv(
 # # =========================
 
 
-print(super_store_df.head())
-print(super_store_df.shape)
-print(super_store_df.info())
-print(super_store_df.isnull().sum())
-print("Duplicates:", super_store_df.duplicated().sum())
+# print(super_store_df.head())
+# print(super_store_df.shape)
+# print(super_store_df.info())
+# print(super_store_df.isnull().sum())
+# print("Duplicates:", super_store_df.duplicated().sum())
+
+#The data is already cleaned
 
 # # =========================
 # #  KEY METRICS
@@ -30,6 +34,8 @@ print("Duplicates:", super_store_df.duplicated().sum())
 
 total_sales = super_store_df['Sales'].sum()
 total_profit = super_store_df['Profit'].sum()
+print("Total Sales:", total_sales)
+print("Total Profit:", total_profit)
 
 # # =========================
 # #  SALES ANALYSIS
@@ -39,18 +45,20 @@ sales_by_category = super_store_df.groupby('Category')['Sales'].sum()
 sales_by_region= super_store_df.groupby('Region')['Sales'].sum()
 sales_by_state = super_store_df.groupby('State')['Sales'].sum()
 
+
 # # =========================
 # #  PROFIT ANALYSIS
 # # =========================
 
 profit_by_category = super_store_df.groupby('Category')['Profit'].sum()
 profit_by_subcategory = super_store_df.groupby('Sub-Category')['Profit'].sum()
-profit_by_proucts = super_store_df.groupby('Product Name')['Profit'].sum()
-profit_by_customer_segment = super_store_df.groupby('Segment')['Profit']
+profit_by_proucts = super_store_df.groupby('Product Name')['Profit'].sum().sort_values()
+profit_by_customer_segment = super_store_df.groupby('Segment')['Profit'].sum()
 
 
-
-
+lossing_products = profit_by_proucts[
+    (profit_by_proucts < 0)
+].head(40)
 
 
 # # =========================
@@ -66,8 +74,26 @@ print(Ship_mode)
 # . Charts
 # =========================
 
+# # Profit By Sub-Category
 
+profit_by_subcategory = (
+    super_store_df.groupby("Sub-Category")["Profit"]
+    .sum()
+    .sort_values(ascending=False)
+)
 
+plt.figure(figsize=(12, 6))
+
+profit_by_subcategory.plot(kind="bar")
+
+plt.title("Profit by Sub-Category")
+plt.xlabel("Sub-Category")
+plt.ylabel("Total Profit")
+plt.xticks(rotation=45, ha="right")
+plt.tight_layout()
+
+# plt.savefig("Visuals/profit_by_subcategory.png")
+# plt.show()
 # #  DISCOUNT ANALYSIS
 
 
@@ -88,10 +114,10 @@ plt.title("Average Profit by Discount")
 plt.xlabel("Discount")
 plt.ylabel("Average Profit")
 plt.tight_layout()
-plt.show()
+# plt.show()
 
 
-# #  Changed Sales Over time 
+
 
 # By The Month:
 super_store_df["Order Date"] = pd.to_datetime(super_store_df["Order Date"])
@@ -115,7 +141,7 @@ plt.xlabel("Date")
 plt.ylabel("Sales")
 
 plt.tight_layout()
-plt.show()
+# plt.show()
 
 #By the Year
 
@@ -132,31 +158,44 @@ sales_growth = yearly_sales.pct_change() * 100
 print(sales_growth)
 
 
+
+# Worst performing sub-category
+print('Worst Performing Sub-Category',profit_by_subcategory.sort_values().head())
+
+# Worst performing region
+print('Worst performing region',sales_by_region.sort_values().head())
+
+# Worst performing state
+print('Worst performing state',sales_by_state.sort_values().head())
+
 # =========================
 # . OUTPUT
 # =========================
 
+print(sales_by_state.max())
 
-# print("Total Sales:", total_sales)
-# print("Total Profit:", total_profit)
 
-# print("\nSales by Category:")
-# print(sales_by_category)
 
-# print("\nProfit by Category:")
-# print(profit_by_category)
+print("\nSales by Category:")
+print(sales_by_category)
+
+print("\nProfit by Category:")
+print(profit_by_category)
 
 print("\nProfit by Sub-Category:")
 print(profit_by_subcategory)
 
-# print("\nProfit by Product:")
-# print(profit_by_proucts)
+print("\nProfit by Product:")
+print(profit_by_proucts)
 
-# print("\nProfit by Customer Segment:")
-# print(profit_by_customer_segment)
+print("\nProfit by Customer Segment:")
+print(profit_by_customer_segment)
 
-# print("\nSales by Region:")
-# print(sales_by_region)
+print("\nSales by Region:")
+print(sales_by_region)
 
-# print("\nSales by State:")
-# print(sales_by_state)
+print("\nSales by State:")
+print(sales_by_state)
+
+print("\nLoss Generator Products:")
+print(lossing_products)
---
+++ src/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+plt.savefig("Visuals/profit_by_subcategory.png")
+# plt.show()
